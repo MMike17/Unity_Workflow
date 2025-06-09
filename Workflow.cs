417631cc8b5efde2400ba6f0b52b2bbe3c365d98
MMike17 <matthews.r.mike@gmail.com>
2025-06-09 19:58:27 +0200

    Split scripts

--- Workflow.cs
+++ Workflow.cs
@@ -401,28 +401,3 @@ public class Workflow : EditorWindow
 		AssetDatabase.ImportAsset(MakeProjectRelative(scriptPath));
 	}
 }
-
-/// <summary>Popup for creation of feature development environment</summary>
-class WorkflowInputPopup : PopupWindowContent
-{
-	string folderName;
-
-	public override void OnOpen()
-	{
-		base.OnOpen();
-		folderName = "";
-	}
-
-	public override void OnGUI(Rect rect)
-	{
-		EditorGUILayout.LabelField("Feature name", Workflow.TitleStyle);
-		EditorGUILayout.Space();
-
-		folderName = EditorGUILayout.TextField(folderName);
-
-		EditorGUILayout.Space();
-
-		if (GUILayout.Button("Create"))
-			Workflow.CreateDevFeature(folderName);
-	}
-}
